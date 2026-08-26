eb7296f5a82ee880c4194e306fa45b2695d5c403
andreibarakovskii-spec <andreibarakovskii@gmail.com>
2026-08-26 07:58:59 +0300

    Bump Martin 1:1 concept build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.imagine.martinhost.fixed"
         minSdk = 31
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.5.0-3d-npc"
+        versionCode = 6
+        versionName = "0.6.0-concept-1to1"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
