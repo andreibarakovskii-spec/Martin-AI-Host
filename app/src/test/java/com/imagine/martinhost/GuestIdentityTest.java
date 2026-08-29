5d46d63860f243ba81f38abaae979677beaacfc1
andreibarakovskii-spec <andreibarakovskii@gmail.com>
2026-08-29 14:24:19 +0300

    Test conservative camera-name extraction

--- app/src/test/java/com/imagine/martinhost/GuestIdentityTest.java
+++ app/src/test/java/com/imagine/martinhost/GuestIdentityTest.java
@@ -3,4 +3,7 @@ import org.junit.Test;import static org.junit.Assert.*;
 public class GuestIdentityTest {
  @Test public void commonSpeechWordsAreNotNewNames(){assertFalse(GuestStore.isPlausibleNewName("не"));assertFalse(GuestStore.isPlausibleNewName("готов"));assertFalse(GuestStore.isPlausibleNewName("всего"));}
  @Test public void properNameIsAccepted(){assertTrue(GuestStore.isPlausibleNewName("Андрей"));}
+ @Test public void explicitIntroductionExtractsName(){assertEquals("Андрей",GuestIdentity.explicitName("Меня зовут Андрей."));assertEquals("Катя",GuestIdentity.explicitName("Я Катя, рада познакомиться"));assertEquals("Игорь",GuestIdentity.explicitName("Игорь"));}
+ @Test public void listOfNamesCannotBecomeCameraIdentity(){assertEquals("",GuestIdentity.explicitName("Имена, Игорь, Катя, Сергей, Андрей."));assertEquals("",GuestIdentity.explicitName("Катя, Сергей"));}
+ @Test public void ordinarySentenceIsNotIntroduction(){assertEquals("",GuestIdentity.explicitName("Я готов"));assertEquals("",GuestIdentity.explicitName("Я всего лишь здесь один"));}
 }
